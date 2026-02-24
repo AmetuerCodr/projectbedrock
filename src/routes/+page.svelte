65049e06ab77d85ce6d29b8fc81083f251dab510
Pseudo_Supreme <gadgetguy49@gmail.com>
2026-02-24 14:11:28 -0500

    initial commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,3 @@
+<h1>Welcome to your library project</h1>
+<p>Create your package using @sveltejs/package and preview/showcase your work with SvelteKit</p>
+<p class="text-green-400 bg-slate-900" >Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
